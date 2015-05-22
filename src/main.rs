8b1d924da0f846f66a143f61ce104d6da27866f8
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-22 00:37:34 -0700

    Generate N by P random solutions for an initial population
    Have to use slices because arrays implement traits only up to size 32
    
    Could replace with vectors for a "small change in the constant cost of
    the program, it wouldn't make running the calculations slower or
    anything." Would clean up the iteration code, but the initialization may
    be a bit uglier.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+extern crate rand;
+
+use rand::distributions::{IndependentSample, Range};
+
 const N: usize = 50;
+const P: usize = 128;
 
 fn schwefel(solution: &[f64; N]) -> f64 {
     418.9829_f64 * N as f64
@@ -6,5 +11,22 @@ fn schwefel(solution: &[f64; N]) -> f64 {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let range = Range::new(-512.03, 511.97);
+
+    let mut population = [[0_f64; N]; P];
+
+    for solution in &mut population[..] {
+        for x in &mut solution[..] {
+            *x = range.ind_sample(&mut rng);
+        }
+    }
+
+    // debug print of solutions
+    for solution in &population[..] {
+        for x in &solution[..] {
+            println!("{} ", x);
+        }
+    }
+    println!("The best solution's fitness was {}", schwefel(&population[0]));
 }
