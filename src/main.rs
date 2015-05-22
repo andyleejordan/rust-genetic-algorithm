5afca1cf43a97ab62035ae5135122ad0c17e728b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-22 00:36:39 -0700

    Return last value impliticly
    Better style, thanks to kimundi

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 const N: usize = 50;
 
 fn schwefel(solution: &[f64; N]) -> f64 {
-    return 418.9829_f64 * N as f64
-        + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin());
+    418.9829_f64 * N as f64
+        + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin())
 }
 
 fn main() {
