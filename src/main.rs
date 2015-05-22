899bb68f3cda1938d88864d76adad6e169697aa5
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-21 22:59:47 -0700

    Swap Vec<f64> with [f64, N]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-fn schwefel(solution: &Vec<f64>) -> f64 {
-    return 418.9829_f64 * solution.len() as f64
+const N: usize = 50;
+
+fn schwefel(solution: &[f64; N]) -> f64 {
+    return 418.9829_f64 * N as f64
         + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin());
 }
 
