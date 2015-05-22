b624a08eda78800f8d2154b533bab3c2bff1754b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-21 22:59:05 -0700

    Add Schwefel function for Vec<f64>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+fn schwefel(solution: &Vec<f64>) -> f64 {
+    return 418.9829_f64 * solution.len() as f64
+        + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin());
+}
+
 fn main() {
     println!("Hello, world!");
 }
