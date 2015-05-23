18d95e074bc25aab7e8ff77c2f0367757996fdcd
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-22 21:13:42 -0700

    Initialize Vector of Vectors using from_iter
    Thanks to mbrubeck and bstrie

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate rand;
 
+use std::iter::FromIterator;
 use rand::distributions::{IndependentSample, Range};
 
 const N: usize = 50;
 const P: usize = 128;
 
-fn schwefel(solution: &[f64; N]) -> f64 {
-    418.9829_f64 * N as f64
+fn schwefel(solution: &Vec<f64>) -> f64 {
+    418.9829_f64 * solution.len() as f64
         + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin())
 }
 
@@ -14,13 +15,13 @@ fn main() {
     let mut rng = rand::thread_rng();
     let range = Range::new(-512.03, 511.97);
 
-    let mut population = [[0_f64; N]; P];
+    // initialize population
+    let population = Vec::from_iter((0..P).map(|_| {
+        Vec::from_iter((0..N).map(|_| {
+            range.ind_sample(&mut rng)
+        }))
+    }));
 
-    for solution in &mut population[..] {
-        for x in &mut solution[..] {
-            *x = range.ind_sample(&mut rng);
-        }
-    }
 
     // debug print of solutions
     for solution in &population[..] {
