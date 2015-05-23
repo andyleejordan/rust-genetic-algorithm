fb48a3c9d93b957003235d089172cf8bd9893c8f
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-23 14:57:29 -0700

    Move Individual into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,7 @@
-extern crate rand;
-
-use std::cmp::{Eq, PartialEq, Ordering, PartialOrd};
 use std::iter::FromIterator;
-use rand::distributions::{IndependentSample, Range};
+use individual::Individual;
 
-fn schwefel(solution: &Vec<f64>) -> f64 {
-    418.9829_f64 * solution.len() as f64
-        + solution.iter().fold(0_f64, |sum, x| sum + x * x.abs().sqrt().sin())
-}
-
-struct Individual {
-    solution: Vec<f64>,
-    fitness: f64,
-}
-
-impl Individual {
-    fn new() -> Individual {
-        let n = 50;
-        let mut rng = rand::thread_rng();
-        let range = Range::new(-512.03_f64, 511.97);
-
-        let solution = Vec::from_iter((0..n).map(|_| range.ind_sample(&mut rng)));
-        let fitness = schwefel(&solution);
-
-        Individual { solution: solution, fitness: fitness }
-    }
-}
-
-impl Eq for Individual {}
-
-impl Ord for Individual {
-    /// This dangerously delegates to `partial_cmp`
-    fn cmp(&self, other: &Self) -> Ordering {
-        if let Some(result) = self.fitness.partial_cmp(&other.fitness) {
-            return result;
-        }
-        unimplemented!();
-    }
-}
-
-impl PartialEq<Individual> for Individual {
-    /// This doesn't use `fitness.eq()` because it needs to be
-    /// consistent with `Eq`
-    fn eq(&self, other: &Individual) -> bool {
-        if let Some(result) = self.fitness.partial_cmp(&other.fitness) {
-            return result == Ordering::Equal;
-        }
-        unimplemented!();
-    }
-}
-
-impl PartialOrd for Individual {
-    /// This delegates to `fitness.partial_cmp()`
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.fitness.partial_cmp(&other.fitness)
-    }
-}
+mod individual;
 
 fn main() {
     let pop_size = 128;
