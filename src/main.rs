313b66eb7162195f5980272184f39ade22df9b0c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-24 01:54:45 -0700

    Replace population on each iteration
    Previously I wasn't mutating the prior generation, only ever the initial
    population. This makes more sense.

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ fn main() {
     let range = Range::new(-512.03_f64, 511.97); // range for Schwefel problem
 
     // initialize population
-    let population: Vec<_> = (0..128).map(|_| {
+    let mut population: Vec<_> = (0..128).map(|_| {
         Individual::new(&range, &mut rng)
     }).collect();
 
     for i in 0..10000 {
         // generate mutated offspring
-        let offspring: Vec<_> = population.iter().map(|x| {
+        population = population.iter().map(|x| {
             x.mutate(&range, &mut rng)
         }).collect();
 
-        let best = offspring.iter().min().unwrap();
         println!("{}th fitness: {}", i, best.fitness);
+        let best = population.iter().min().unwrap();
 
         if best.fitness < 1000_f64 {
             println!("Solution: {:?}", best.solution);
