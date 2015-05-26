69a1b3cd8d0166e0f6e1982fcccd828657d9dd41
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-05-26 01:39:07 -0700

    Optionally specify problems with CLI
    Refactored the usage string.

--- src/main.rs
+++ src/main.rs
@@ -27,18 +27,20 @@ fn main() {
         .author("Andrew Schwartzmeyer <andrew@schwartzmeyer.com")
         .about("A genetic algorithm in Rust for Schwefel's function.")
         .args_from_usage(
-            "[<problem>] 'The problem to solve'
-             [-d --dimension <30>] 'Sets the dimension of the hypercube'
-             [-p --population <256>] 'Sets the size of the population'
-             [-i --iterations <5000>] 'Sets maximum number of generations'
+            "[problem]... 'The problem to solve'
+             -d --dimension [30] 'Sets the dimension of the hypercube'
+             -p --population [256] 'Sets the size of the population'
+             -i --iterations [5000] 'Sets maximum number of generations'
              [verbose]... -v 'Print fitness (1) and solution (2) every 10th generation'")
         .get_matches();
-    let problem = value_t!(matches.value_of("problem"), Problem)
-        .unwrap_or(Problem::Schwefel);
+    let problems = value_t!(matches.values_of("problem"), Problem)
+        .unwrap_or(vec![Problem::Schwefel, Problem::Ackley]);
     let dimension = value_t!(matches.value_of("d"), usize).unwrap_or(30);
     let population = value_t!(matches.value_of("p"), usize).unwrap_or(256);
     let iterations = value_t!(matches.value_of("i"), usize).unwrap_or(5000);
     let verbosity = matches.occurrences_of("verbose") as usize;
 
-    algorithm::search(problem, 0.05_f64, dimension, population, iterations, verbosity)
+    for problem in problems {
+        algorithm::search(problem, 0.05_f64, dimension, population, iterations, verbosity)
+    }
 }
